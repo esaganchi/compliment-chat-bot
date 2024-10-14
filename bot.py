082d745d8de54cd127598f321581337e88e9ab99
Evgenii Saganchi <146103733+esaganchi@users.noreply.github.com>
2024-10-14 22:21:52 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ import nest_asyncio
 nest_asyncio.apply()
 
 # Папка для изображений
-IMAGES_DIR = 'https://github.com/esaganchi/compliment-chat-bot/Images'
+IMAGES_DIR = 'Images'
 
 # Файл для хранения комплиментов, отправленных за текущий день
 SENT_COMPLIMENTS_FILE = 'sent_compliments.json'
